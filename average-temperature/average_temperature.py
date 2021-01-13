e51fac8d52833ad84a8394e4ebb404c3077c5df9
Mark Ambrus <ambrusmark97@gmail.com>
2021-01-13 18:34:22 +0100

    Average temperature task done

--- average-temperature/average_temperature.py
+++ average-temperature/average_temperature.py
@@ -4,31 +4,31 @@ def get_extremes(file):
     try:
         with open(file, 'r') as file:
             content = file.read()
-            #print(content)
-            #content.remove(content[0])
             info = content.split('\n')
             data_pool = []
             for line in info:
                 data = line.split(' ')
                 data_pool.append(data)
-            #print(data_pool)
             first = data_pool[0][0]
             second = data_pool[0][1]
             third = data_pool[0][2]
-            coldest_year_in_first = 0
-            hottest_year_in_first = 1
-            coldest_year_in_second = 2
-            hottest_year_in_second = 3
-            coldest_year_in_third = 4
-            hottest_year_in_third = 5
-            #for i in range(len(data_pool))
+            coldest_years = [data_pool[1][3], data_pool[1][3], data_pool[1][3]]
+            hottest_years = [data_pool[1][3], data_pool[1][3], data_pool[1][3]]
+            coldest_values = [data_pool[1][0], data_pool[1][1], data_pool[1][2]]
+            hottest_values = [data_pool[1][0], data_pool[1][1], data_pool[1][2]]
+            for i in range(3):
+                for j in range(1, len(data_pool)):
+                    if data_pool[j][i] < coldest_values[i]:
+                        coldest_years[i] = data_pool[j][3]
+                        coldest_values[i] = data_pool[j][i]
+                    if data_pool[j][i] > hottest_values[i]:
+                        hottest_years[i] = data_pool[j][3]
+                        hottest_values[i] = data_pool[j][i]
             res = {}
-            res[first] = [coldest_year_in_first, hottest_year_in_first]
-            res[second] = [coldest_year_in_second, hottest_year_in_second]
-            res[third] = [coldest_year_in_third, hottest_year_in_third]
-
+            res[first] = [coldest_years[0], hottest_years[0]]
+            res[second] = [coldest_years[1], hottest_years[1]]
+            res[third] = [coldest_years[2], hottest_years[2]]
             return res
-
     except NameError:
         return 'Name Error'
     except FileNotFoundError:
@@ -36,4 +36,10 @@ def get_extremes(file):
     except IOError:
         return 'IO Error'
 
-print(get_extremes(temperatures_file))
+for k, v in (get_extremes(temperatures_file)).items():
+    value = ''
+    for element in v:
+        value += element
+        if v.index(element) < len(v) - 1:
+            value += ', '
+    print(str(k) + ' => ' + str(value))
